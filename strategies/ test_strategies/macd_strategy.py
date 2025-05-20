2d7ddb902017ace611799daba56852076e165b52
liNDRof <@email.com>
2025-05-20 18:25:55 +0200

    asddddaa

--- strategies/ test_strategies/macd_strategy.py
+++ strategies/ test_strategies/macd_strategy.py
@@ -5,12 +5,10 @@ from binance.client import Client
 df['macd'] = ta.trend.macd(df['close'])
 df['macd_signal'] = ta.trend.macd_signal(df['close'])
 
-# LONG сигнал
-df['long_signal'] = (df['macd'] > df['macd_signal']) & \
-                    (df['macd'].shift(1) <= df['macd_signal'].shift(1)) & \
-                    (df['macd'] < 0)
+df['buy_signal'] = (df['macd'] > df['macd_signal']) & \
+                   (df['macd'].shift(1) <= df['macd_signal'].shift(1)) & \
+                   (df['macd'] < 0)
 
-# SHORT сигнал
-df['short_signal'] = (df['macd'] < df['macd_signal']) & \
-                     (df['macd'].shift(1) >= df['macd_signal'].shift(1)) & \
-                     (df['macd'] > 0)
+df['sell_signal'] = (df['macd'] < df['macd_signal']) & \
+                    (df['macd'].shift(1) >= df['macd_signal'].shift(1)) & \
+                    (df['macd'] > 0)
